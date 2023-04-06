c62e1ee0acc21125bcf4d8ad2350ef0fb7fac41d
WenHao1223 <wenhaojshs@gmail.com>
2023-04-07 01:33:56 +0800

    firebase added to projects/innovations

--- projects/innovations/innovations.js
+++ projects/innovations/innovations.js
@@ -1,7 +1,8 @@
 const db = firebase.firestore();
 const storageRef = firebase.storage().ref().child('projects/innovations');
 
-db.collection("public/projects/innovations").get().then(async (innovationsRef) => {
+db.collection("public/projects/innovations").orderBy("createdYear").get().then(async (innovationsRef) => {
+    let indexNo = 1;
     innovationsRef.forEach((innovationDoc) => {
         // console.log(innovationDoc.id, "=>", innovationDoc.data());
         innovationData = innovationDoc.data();
@@ -11,10 +12,16 @@ db.collection("public/projects/innovations").get().then(async (innovationsRef) =
         }
         else if (innovationData["divStyle"] == "fullScreen"){
             $(".navbar_section").html($(".navbar_section").html() + `<div class="card"><img src="" id="${innovationDoc.id}" alt="${innovationData['name']}" class="card-img-top"><div class="card-body"><h3 class="card-title">${innovationData['name']}<span class="card-subheader">(${innovationData['createdYear']})</span></h3><p class="card-text">${innovationData['des']}</p><a href="?id=${innovationDoc.id}" class="card-link"><i>Read More</i></a></div></div>`);
+        } else if (innovationData["divStyle"] == "vertical") {
+            if (indexNo % 2){
+                $(".navbar_section").html($(".navbar_section").html() + `<div class="row"><div class="col-xl-6 xl_index_${indexNo}"><div class="card"><img src="" id="${innovationDoc.id}" alt="${innovationData['name']}" class="card-img-top"><div class="card-body"> <h3 class="card-title">${innovationData['name']}<span class="card-subheader">(${innovationData['createdYear']})</span></h3><p class="card-text">${innovationData['des']}</p><a href="?id=${innovationDoc.id}" class="card-link"><i>Read More</i></a></div></div></div><div class="col-xl-6 xl_index_${++indexNo}"></div>`);
+            } else {
+                $(`.xl_index_${indexNo}`).html(`<div class="card"><img src="" id="${innovationDoc.id}" alt="${innovationData['name']}" class="card-img-top"><div class="card-body"> <h3 class="card-title">${innovationData['name']}<span class="card-subheader">(${innovationData['createdYear']})</span></h3><p class="card-text">${innovationData['des']}</p><a href="?id=${innovationDoc.id}" class="card-link"><i>Read More</i></a></div></div></div>`);
+                indexNo++;
+            }
         }
 
         storageRef.child(innovationDoc.id+".png").getDownloadURL().then((photoURL) => {
-            console.log("loaded");
             $(`img#${innovationDoc.id}`).attr("src", photoURL);
         }).catch((error) => {
             console.error("No such file - projects/innovations: ", error);
