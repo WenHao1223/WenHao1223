d0447c44194bd335958f0ae6c8ca42fa80b550c3
WenHao1223 <wenhaojshs@gmail.com>
2023-04-26 16:16:31 +0800

    add new files

--- projects/innovations/innovations.js
+++ projects/innovations/innovations.js
@@ -1,6 +1,7 @@
 const db = firebase.firestore();
 const storageRef = firebase.storage().ref().child('projects/innovations');
 
+// Load data from firebase
 db.collection("public/projects/innovations").orderBy("createdYear").get().then(async (innovationsRef) => {
     let indexNo = 1;
     innovationsRef.forEach((innovationDoc) => {
@@ -8,7 +9,7 @@ db.collection("public/projects/innovations").orderBy("createdYear").get().then(a
         innovationData = innovationDoc.data();
 
         if (innovationData["divStyle"] == "horizontal"){
-            $(".navbar_section").html($(".navbar_section").html() + `<div class="card"><div class="row"><div class="col-sm-12 col-md-12 col-lg-4"><img src="" id="${innovationDoc.id}" alt="${innovationData['name']}" class="img-fluid"></div><div class="col-sm-12 col-md-12 col-lg-7"><div class="card-body"><h3 class="card-title">${innovationData['name']}<span class="card-subheader">(${innovationData['createdYear']})</span></h3><p class="card-text">${innovationData['des']}</p><a href="?id=${innovationDoc.id}" class="card-link"><i>Read More</i></a></div></div></div></div>`);
+            $(".navbar_section").html($(".navbar_section").html() + `<div class="card vertical"><div class="row"><div class="col-sm-12 col-md-12 col-lg-4"><img src="" id="${innovationDoc.id}" alt="${innovationData['name']}" class="img-fluid"></div><div class="col-sm-12 col-md-12 col-lg-7"><div class="card-body"><h3 class="card-title">${innovationData['name']}<span class="card-subheader">(${innovationData['createdYear']})</span></h3><p class="card-text">${innovationData['des']}</p><a href="?id=${innovationDoc.id}" class="card-link"><i>Read More</i></a></div></div></div></div>`);
         }
         else if (innovationData["divStyle"] == "fullScreen"){
             $(".navbar_section").html($(".navbar_section").html() + `<div class="card"><img src="" id="${innovationDoc.id}" alt="${innovationData['name']}" class="card-img-top"><div class="card-body"><h3 class="card-title">${innovationData['name']}<span class="card-subheader">(${innovationData['createdYear']})</span></h3><p class="card-text">${innovationData['des']}</p><a href="?id=${innovationDoc.id}" class="card-link"><i>Read More</i></a></div></div>`);
@@ -21,7 +22,7 @@ db.collection("public/projects/innovations").orderBy("createdYear").get().then(a
             }
         }
 
-        storageRef.child(innovationDoc.id+".png").getDownloadURL().then((photoURL) => {
+        storageRef.child(innovationDoc.id+"/photo.png").getDownloadURL().then((photoURL) => {
             $(`img#${innovationDoc.id}`).attr("src", photoURL);
         }).catch((error) => {
             console.error("No such file - projects/innovations: ", error);
