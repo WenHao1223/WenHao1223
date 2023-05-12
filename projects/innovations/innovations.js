56d2b4417a56d9f89b50ef45de9b7256c7f57001
WenHao1223 <wenhaojshs@gmail.com>
2023-05-12 18:03:18 +0800

    done innovation page

--- projects/innovations/innovations.js
+++ projects/innovations/innovations.js
@@ -6,6 +6,8 @@ paramID = param.get("id");
 
 // paramID is null
 if(!paramID){
+    $("#b_back").addClass("d-none");
+
     // Load data from firebase
     db.collection("public/projects/innovations").orderBy("createdYear").get().then(async (innovationsRef) => {
         let indexNo = 1;
@@ -34,15 +36,42 @@ if(!paramID){
             });
         });
     }).catch((error) => {
-        console.error("Error getting list of documents inside innovations: ", error);
+        console.error("Error getting list of documents inside innovations:", error);
     });    
 } else {
-    console.log(paramID);
+    $("#b_back").removeClass("d-none");
+
+    db.collection("public/projects/innovations").doc(paramID).get().then((docSpecific) => {
+        if (docSpecific.exists) {
+            docSpecificData = docSpecific.data();
+
+            $(".navbar_section").html($(".navbar_section").html() + `<div class="card"><img src="" id="${paramID}" alt="${paramID}" class="card-img-top"><div class="card-body"><h3 class="card-title">${docSpecificData["name"]}<span class="card-subheader">(${docSpecificData["createdYear"]})</span></h3><p class="card-text">${docSpecificData["des"]}</p><a class="card-link" data-bs-toggle="collapse" href="#readMore" role="button" aria-expanded="false" aria-controls="collapseExample"><i>Read More</i></a><div class="collapse mt-3" id="readMore"><div class="card card-body text-bg-dark">${docSpecificData["longEssay"]}</div></div><div class="row" style="width: calc(100vw - var(--padding) * 2);"><div class="col col-md-12 mt-5"><ul class="list-group list-group-horizontal mb-3 horizontal_scrollbar"></ul><ul class="list-group list-group-horizontal mb-3 horizontal_scrollbar"></ul></div><div class="col col-md-12"></div></div></div></div>`);
+
+            storageRef.child(paramID+"/photo.png").getDownloadURL().then((photoURL) => {
+                $(`img#${paramID}`).attr("src", photoURL);
+            }).catch((error) => {
+                console.error("No such file - projects/innovations with ID of", paramID, ": ", error);
+            });
+            
+            const gallery = {"first":"images", "last": "attachments"};
+            for(let i = 0; i < Object.keys(gallery).length; i++){
+                for(let j = 0; j < docSpecificData[gallery[Object.keys(gallery)[i]]].length; j++){
+                    eval(`$(".list-group").${Object.keys(gallery)[i]}()`).html(eval(`$(".list-group").${Object.keys(gallery)[i]}()`).html() + `<li class="list-group-item"><iframe src="https://drive.google.com/file/d/${docSpecificData[gallery[Object.keys(gallery)[i]]][j]}/preview" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe></li>`);
+                }
+            }
+
+        } else {
+            console.error("No such file - projects/innovations with ID of", paramID);
+            $(".navbar_section").html("File not found.");
+        }
+    }).catch((error) => {
+        console.error("Error getting collection - innovations:", error);
+    });
     
     $("#readMore").on('show.bs.collapse', function(){
         $("#readMore").prev().children().text("Show Less");
-      });
-      $("#readMore").on('hide.bs.collapse', function(){
-          $("#readMore").prev().children().text("Read More");
-      });
+    });
+    $("#readMore").on('hide.bs.collapse', function(){
+        $("#readMore").prev().children().text("Read More");
+    });
 }
