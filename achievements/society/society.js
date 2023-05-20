592a219cbe430d95dbd4f2fa7e549349b0205528
WenHao1223 <wenhaojshs@gmail.com>
2023-05-21 03:05:40 +0800

    add files

--- achievements/society/society.js
+++ achievements/society/society.js
@@ -1,19 +1,19 @@
 const db = firebase.firestore();
-const firestoreRef = db.collection("public/achievements/competitions");
-const storageRef = firebase.storage().ref().child('achievements/competitions');
+const firestoreRef = db.collection("public/achievements/society");
+const storageRef = firebase.storage().ref().child('achievements/society');
 
-ls_level = [], ls_year = [];
+ls_club = [], ls_year = [];
 firestoreRef.orderBy("year").get().then(async (querySnapshot) => {
     await querySnapshot.forEach((doc) => {
-        ls_level.push(doc.data()["level"]);
+        ls_club.push(doc.data()["club"]);
         ls_year.push(doc.data()["year"]);
     });
-    ls_level = Array.from(new Set(ls_level)).sort();
+    ls_club = Array.from(new Set(ls_club)).sort();
     ls_year = Array.from(new Set(ls_year)).reverse();
 
     load_ls();
 }).catch((error) => {
-    console.error("Error getting list of documents inside query snapshot of competitions:", error);
+    console.error("Error getting list of documents inside query snapshot of society:", error);
 });
 
 $(".btn-group").change(function(){
@@ -25,32 +25,28 @@ function load_ls(){
     for(let i = 0; i < eval("ls_"+$("input[name='btnradio']:checked")[0].id).length; i++){
         firestoreRef.where($("input[name='btnradio']:checked")[0].id, "==", eval("ls_"+$("input[name='btnradio']:checked")[0].id)[i]).get().then(async (querySnapshot) => {
             card_html += `<h3 class="header mt-4 mb-4">${eval("ls_"+$("input[name='btnradio']:checked")[0].id)[i]}</h3><div class="row g-0">`;
-            await querySnapshot.forEach(async (competitionDoc) => {
-                competitionData = competitionDoc.data();
+            await querySnapshot.forEach(async (societyDoc) => {
+                societyData = societyDoc.data();
         
-                card_html += `<div class="col col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12"><div class="card"><img id="${competitionDoc.id}" src="" class="card-img-top" alt="${competitionData["name"]}"><div class="card-body"><h5 class="card-title">${competitionData["name"]}</h5><p class="card-text">`;
+                card_html += `<div class="col col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12"><div class="card"><img id="${societyDoc.id}" src="" class="card-img-top" alt="${societyData["name"]}"><div class="card-body"><h5 class="card-title">${societyData["name"]}</h5><p class="card-text">`;
 
                 award_dict = {"Gold": "🥇", "Silver": "🥈", "Bronze": "🥉", "Participation": "📃"};
-                for(let i = 0; i < competitionData["award"].length; i++){
-                    if(competitionData["award"][i] in award_dict){
-                        card_html += award_dict[competitionData["award"][i]];
+                for(let i = 0; i < societyData["award"].length; i++){
+                    if(societyData["award"][i] in award_dict){
+                        card_html += award_dict[societyData["award"][i]];
                     } else {
                         card_html += `🏆`;
                     }
-                    card_html += ` ${competitionData["award"][i]}<br>`;
+                    card_html += ` ${societyData["award"][i]}<br>`;
                 }
 
-                card_html += `</p><p class="card-text card_t_des"><i>${competitionData["teammates"].join(", ")}</i>`;
-                if(!!competitionData["proj"]){
-                    card_html += `<br><i>Project won: ${competitionData["proj"]}</i>`
-                }
                 card_html += `</p></div></div></div>`;
 
-                storageRef.child(competitionDoc.id+".png").getDownloadURL().then((photoURL) => {
-                    $(`img#${competitionDoc.id}`).attr("src", photoURL);
+                storageRef.child(societyDoc.id+".png").getDownloadURL().then((photoURL) => {
+                    $(`img#${societyDoc.id}`).attr("src", photoURL);
                 }).catch((error) => {
                     firebase.storage().ref('achievements/null.png').getDownloadURL().then((photoURL) => {
-                        $(`img#${competitionDoc.id}`).attr("src", photoURL);
+                        $(`img#${societyDoc.id}`).attr("src", photoURL);
                     }).catch((error) => {
                         console.error("null.png not found: ", error);
                     });
@@ -64,7 +60,7 @@ function load_ls(){
             $(".content").html(card_html);
         })
         .catch((error) => {
-            console.error("Error getting documents of level / year of query snapshot of competitions: ", error);
+            console.error("Error getting documents of club / year of query snapshot of society: ", error);
         });
     }
 }
